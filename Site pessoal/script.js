358b71f17b5cc7e3fa8a462dd2fdd87b7eed38f8
Gabriel Sakura <73794925+gabsakura@users.noreply.github.com>
2024-08-05 19:48:26 -0300

    Add files via upload

--- Site pessoal/script.js
+++ Site pessoal/script.js
@@ -102,4 +102,3 @@ document.addEventListener("DOMContentLoaded", function() {
         text.style.display = "none";
     });
 });
-
--- Site pessoal/scroll.js
+++ Site pessoal/scroll.js
@@ -13,3 +13,32 @@ sr.reveal('.projetos', {
 });
 
 var menuItem = document.querySelectorAll('.item-menu');
+
+<div class="habilidades">
+<div class="caixas">
+    <div class="Imagem-habilidade" onclick="toggleText(this)">
+        <img src="img/Css-icon.png" alt="Habilidade CSS">
+        <p class="texto-habilidade">
+            Usei minhas habilidades em CSS para criar layouts responsivos e estilizar diversos componentes em vários projetos.
+        </p>
+    </div>
+    <div class="Imagem-habilidade" onclick="toggleText(this)">
+        <img src="img\HTML.png" alt="Habilidade HTML">
+        <p class="texto-habilidade">
+            Usei minhas habilidades em HTML para estruturar conteúdo de maneira semântica e acessível em vários projetos.
+        </p>
+    </div>
+    <div class="Imagem-habilidade" onclick="toggleText(this)">
+        <img src="img\JS.png" alt="Habilidade JavaScript">
+        <p class="texto-habilidade">
+            Usei minhas habilidades em JavaScript para adicionar interatividade e dinamismo em diversas aplicações web.
+        </p>
+    </div>
+    <div class="Imagem-habilidade" onclick="toggleText(this)">
+        <img src="img\Python.png" alt="Habilidade Python">
+        <p class="texto-habilidade">
+            Usei minhas habilidades em Python para construir interfaces de usuário reutilizáveis e eficientes em projetos web.
+        </p>
+    </div>
+</div>
+</div>
